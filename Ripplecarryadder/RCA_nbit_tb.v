2ae34df277c0fa76ccc5d8a50a3d1d0447dae6a7
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 19:02:43 +0530

    Update RCA_nbit_tb.v

--- Ripplecarryadder/RCA_nbit_tb.v
+++ Ripplecarryadder/RCA_nbit_tb.v
@@ -1,1 +1,14 @@
-
+module RCA_nbit_tb;
+parameter n = 16;
+reg [n-1:0]a,b;
+reg cin;
+wire [n-1:0]sum;
+wire cout;
+RCA_nbit dut (a,b,cin,sum,cout);
+initial
+begin
+    a = 16'h26c7;
+    b = 16'hd9c1;
+    #10 $finish;
+end
+endmodule
