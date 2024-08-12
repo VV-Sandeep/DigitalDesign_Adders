e39d216a33d99a54bb7379fb4738fd889334ccfb
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 19:03:33 +0530

    Update CLA_nbit_tb.v

--- carrylookaheadadder/CLA_nbit_tb.v
+++ carrylookaheadadder/CLA_nbit_tb.v
@@ -1,1 +1,15 @@
-
+module CLA_nbit_tb();
+parameter n = 16;
+reg [n-1:0]a,b;
+reg cin;
+wire [n-1:0]sum;
+wire cout;
+CLA_nbit dut (a,b,cin,sum,cout);
+initial 
+begin
+    a = 16'hd9c1;
+    b = 16'h26c7;
+    cin = 1'b0;
+    #10 $finish;
+end
+endmodule
