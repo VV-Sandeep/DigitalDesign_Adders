935e4f5585f1bb40d07f502e9ccfa3825b17ec5f
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 19:03:12 +0530

    Update CLA_nbit.v

--- carrylookaheadadder/CLA_nbit.v
+++ carrylookaheadadder/CLA_nbit.v
@@ -1,1 +1,22 @@
-
+module CLA_nbit(a,b,cin,sum,cout);
+parameter n = 16;
+input [n-1:0]a,b;
+input cin;
+output [n-1:0]sum;
+output cout;
+reg[n-1:0]p,g;
+reg[n:0]c;
+integer i;
+always @(*)
+begin
+    c[0] = cin;
+    p = a^b;
+    g = a&b;
+    for (i = 0; i <n ; i = i+1)
+    begin
+    c[i+1] = (p[i] & c[i]) | g[i];
+    end
+end
+assign sum = p ^ c[n-1:0];
+assign cout = c[n];
+endmodule
