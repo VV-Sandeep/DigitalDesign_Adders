7049e558185e54d15b639c7e9abcfdfe3c5edd18
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 19:02:14 +0530

    Update RCA_nbit.v

--- Ripplecarryadder/RCA_nbit.v
+++ Ripplecarryadder/RCA_nbit.v
@@ -1,1 +1,17 @@
-
+module RCA_nbit(a,b,cin,sum,cout);
+parameter n = 16;
+input [n-1:0]a,b;
+input cin;
+output [n-1:0]sum;
+output cout;
+genvar i;
+wire [n:0]c;
+assign c[0] = 1'b0;
+generate 
+    for (i= 0; i < n; i = i+1)
+    begin
+        FA A1(a[i],b[i],c[i],sum[i],c[i+1]);
+    end
+endgenerate
+assign cout = c[n];
+endmodule
