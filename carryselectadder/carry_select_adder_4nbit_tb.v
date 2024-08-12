efad47d37cffe413311a57721bccdcd50e88366b
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 23:31:26 +0530

    Update carry_select_adder_4nbit_tb.v

--- carryselectadder/carry_select_adder_4nbit_tb.v
+++ carryselectadder/carry_select_adder_4nbit_tb.v
@@ -1,1 +1,14 @@
-
+module carry_select_adder_4nbit_tb();
+parameter n = 64;
+reg [n-1:0]a,b;
+reg cin;
+wire [n-1:0]sum;
+wire cout;
+carry_select_adder_4nbit dut(a,b,cin,sum,cout);
+initial
+begin
+  A = 64'h0000000000123456;
+  B = 64'h0000123456789001;
+  #20 $finish;
+end
+endmodule
