4a63bcde39d3c9a2381acf579b4c4cd93b6d3a39
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-08-12 19:05:26 +0530

    Update carry_skip_adder_16it_tb.v

--- carryskipadder/carry_skip_adder_16it_tb.v
+++ carryskipadder/carry_skip_adder_16it_tb.v
@@ -1,1 +1,14 @@
-
+module carry_skip_adder_16bit_tb;
+reg [15:0]a,b;
+reg cin;
+wire [15:0]sum;
+wire cout;
+carry_skip_adder_16bit A1(a,b,cin,sum,cout);
+initial
+begin
+    a = 16'hd9c1;
+    b = 16'h26c7;
+    cin = 0;
+    #10 $finish;
+end
+endmodule
