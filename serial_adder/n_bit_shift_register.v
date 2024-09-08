f0bf2b6e508cbeb3901655377883c5e59d87e251
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:45:23 +0530

    Update n_bit_shift_register.v

--- serial_adder/n_bit_shift_register.v
+++ serial_adder/n_bit_shift_register.v
@@ -1,1 +1,17 @@
-
+module n_bit_shift_register(clk,ctrl,en,data,in,q);
+input clk,ctrl,en;
+input data;
+input [3:0]in;
+output reg[3:0]q;
+always @(posedge clk)
+begin
+    if(en)
+    begin
+       q <= in;
+    end
+    else if(ctrl)
+    begin
+        q <= {data,q[3:1]};
+    end       
+end
+endmodule
